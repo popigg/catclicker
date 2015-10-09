c9ea494fff58b4d8109223b3910a98df7f89640c
popigg <popigg@gmail.com>
2015-10-09 12:45:35 +0200

    including cat 2

--- app/AppController.js
+++ app/AppController.js
@@ -9,16 +9,20 @@
     function AppController() {
         /*jshint validthis: true */
         var vm = this;
-                
+
         vm.clickCount = 0;
         vm.clickIncrement = function() {
         	vm.clickCount = vm.clickCount +1;
         };
 
+        vm.clickCount1 = 0;
+        vm.clickIncrement1 = function() {
+        	vm.clickCount1 = vm.clickCount1 +1;
+        };
+
         activate();
 
-        function activate() {
-        	console.log(vm.clickCount);
+        function activate() {        	
         }
     }
 })();
